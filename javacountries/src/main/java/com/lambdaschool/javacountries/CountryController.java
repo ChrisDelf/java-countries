5d3a439041c3211a976c92d556fead656fb946a2
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-09-09 16:24:58 -0400

    Search By letter is working!

--- javacountries/src/main/java/com/lambdaschool/javacountries/CountryController.java
+++ javacountries/src/main/java/com/lambdaschool/javacountries/CountryController.java
@@ -3,8 +3,11 @@ package com.lambdaschool.javacountries;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/names")
@@ -20,5 +23,16 @@ public class CountryController {
 
         JavacountriesApplication.ourCountryList.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
         return new ResponseEntity<>(JavacountriesApplication.ourCountryList.countryList, HttpStatus.OK);
+    }
+    //localhost:8080/start/{letter}
+    @GetMapping(value = "/start/{letter}",
+            produces = {"application/json"})
+    public ResponseEntity<?> getCountry(
+            @PathVariable
+                    char letter)
+    {
+        ArrayList<Country> rtnCountries = JavacountriesApplication.ourCountryList.
+                findCountires(e -> e.getName().toUpperCase().charAt(0) == Character.toUpperCase(letter));
+        return new ResponseEntity<>(rtnCountries, HttpStatus.OK);
     }
 }
